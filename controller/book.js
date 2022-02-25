739f7532476c579fc80a5cf59fe0c8f4c77d0546
Hafiz ZeeShan <hafizzeeshan138@gmail.com>
2022-02-25 15:47:45 +0500

    Get All books

--- controller/book.js
+++ controller/book.js
@@ -1,4 +1,5 @@
 const Book = require("../models/book");
+const Student = require("../models/student");
 
 exports.createBook = async (req, res) => {
   try {
@@ -8,6 +9,51 @@ exports.createBook = async (req, res) => {
     };
     const book = await Book.create(newBook);
     return res.status(201).json({
+      status: "success",
+      data: book,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+exports.getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    return res.status(200).json({
+      status: "success",
+      books: books.length,
+      data: books,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+};
+
+exports.borrowBook = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    // console.log(book);
+    if (!book) {
+      return res.status(404).json({
+        status: "error",
+        message: "Book not found",
+      });
+    }
+
+    // const user = await Student.findById(req.student._id);
+    // console.log("efef", req.student._id);
+    // console.log("Fcu", user);
+    book.borrowedBy = req.body.borrowedBy;
+    book.DateOfBorrow = Date.now();
+    book.DateOfReturn = req.body.DateOfReturn;
+    await book.save();
+    return res.status(200).json({
       status: "success",
       data: book,
     });
--- routes/book.js
+++ routes/book.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { createBook } = require("../controller/book");
+const { createBook, getAllBooks, borrowBook } = require("../controller/book");
 const router = express.Router();
 
 router.route("/createBook").post(createBook);
+router.route("/getAllBooks").get(getAllBooks);
+router.route("/borrowBook/:id").post(borrowBook);
 
 module.exports = router;
